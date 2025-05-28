46f04078ec13b8ef6176f812bea2054acf8793c5
PanggilSajaLana <maulanakunjati05@gmail.com>
2025-05-28 21:41:11 +0700

    Tambahan caraosel

--- js/script.js
+++ js/script.js
@@ -1,20 +1,35 @@
-document.addEventListener('DOMContentLoaded', function() {
-    const hamburgerBtn = document.querySelector('.hamburger-menu');
-    const mainNav = document.getElementById('mainNav');
+document.addEventListener("DOMContentLoaded", function () {
+  const hamburgerBtn = document.querySelector(".hamburger-menu");
+  const mainNav = document.getElementById("mainNav");
 
-    if (hamburgerBtn && mainNav) {
-        hamburgerBtn.addEventListener('click', function() {
-            hamburgerBtn.classList.toggle('active');
-            mainNav.classList.toggle('active');
-        });
+  if (hamburgerBtn && mainNav) {
+    hamburgerBtn.addEventListener("click", function () {
+      hamburgerBtn.classList.toggle("active");
+      mainNav.classList.toggle("active");
+    });
 
-        mainNav.querySelectorAll('a').forEach(link => {
-            link.addEventListener('click', () => {
-                if (mainNav.classList.contains('active')) {
-                    hamburgerBtn.classList.remove('active');
-                    mainNav.classList.remove('active');
-                }
-            });
-        });
-    }
+    mainNav.querySelectorAll("a").forEach((link) => {
+      link.addEventListener("click", () => {
+        if (mainNav.classList.contains("active")) {
+          hamburgerBtn.classList.remove("active");
+          mainNav.classList.remove("active");
+        }
+      });
+    });
+  }
 });
+
+let currentIndex = 0;
+function moveSlide(direction) {
+  const slides = document.querySelectorAll(".jumbotron-berita");
+  const totalSlides = slides.length;
+  currentIndex = (currentIndex + direction + totalSlides) % totalSlides;
+  const offset = -currentIndex * 100;
+  document.querySelector(
+    ".carousel-inner"
+  ).style.transform = `translateX(${offset}%)`;
+}
+
+setInterval(() => {
+  moveSlide(1);
+}, 3000);
